b1988153e136ad3c6271ceb119b38faa962f146e
Varun Shah <Varun@Varuns-MBP.lan>
2015-06-23 22:37:30 -0700

    Second Commit

---
+++ Glitch_Garden/Assets/musicManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class musicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+	private AudioSource audioSource;
+
+void Awake(){
+		DontDestroyOnLoad (gameObject);
+	}
+
+	void Start(){
+		audioSource = GetComponent<AudioSource>();
+	}
+
+ void OnLevelWasLoaded(int level){
+		AudioClip thisLevelMusic = levelMusicChangeArray[level];
+		if (thisLevelMusic) {
+
+		}
+			;
+	}
